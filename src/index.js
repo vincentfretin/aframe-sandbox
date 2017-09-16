e2da48599b07792eef49220cd9a020f6e2670b70
Vincent Fretin <vincent.fretin@gmail.com>
2017-09-16 12:05:41 +0200

    example with aframe-ar and react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import "react-hot-loader/patch";
-import "aframe";
-import "aframe-animation-component";
-import "aframe-particle-system-component";
 import "babel-polyfill";
-import { Entity, Scene } from "aframe-react";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./AppAR";
 import { AppContainer } from "react-hot-loader";
 
 if (module.hot) {
-  module.hot.accept("./App", () => {
-    const NewApp = require("./App").default; // eslint-disable-line
+  module.hot.accept("./AppAR", () => {
+    const NewApp = require("./AppAR").default; // eslint-disable-line
     ReactDOM.render(
       <AppContainer>
         <NewApp />
