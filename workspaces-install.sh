562a962a666664f5a0701061770d378b15d8ec82
Vincent Fretin <vincent.fretin@gmail.com>
2017-10-29 08:25:42 +0100

    checkout aframe-teleport-controls

--- workspaces-install.sh
+++ workspaces-install.sh
@@ -10,5 +10,9 @@ test -d workspaces/super-hands && (cd workspaces/super-hands; git_pull) || (
     git clone git@github.com:vincentfretin/aframe-super-hands-component workspaces/super-hands && \
     cd workspaces/super-hands && git remote add upstream git@github.com:wmurphyrd/aframe-super-hands-component.git)
 
+test -d workspaces/aframe-teleport-controls && (cd workspaces/aframe-teleport-controls; git_pull) || (
+    git clone git@github.com:vincentfretin/aframe-teleport-controls workspaces/aframe-teleport-controls && \
+    cd workspaces/aframe-teleport-controls && git remote add upstream git@github.com:fernandojsg/aframe-teleport-controls.git)
+
 test -d workspaces/aframe && (cd workspaces/aframe; git_pull) || (
     git clone git@github.com:aframevr/aframe workspaces/aframe)
